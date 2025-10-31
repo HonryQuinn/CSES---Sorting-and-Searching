17b4da1f9b7a41e09cdcf10ced08f265e93fc52e
vboxuser <Hhoqi>
2025-10-31 00:45:42 +0000

    codigo 3

---
+++ FerrisWheel.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int x;
+    int arr[n];
+    int i=0;
+
+    cin >> n >> x;
+
+    for(int i=0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    while(true){
+        if(arr[i]<=10){
+            
+        }
+    }
+}
